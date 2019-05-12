6adf517a9d49c0ff9d71ce0d486ed10fae647055
rgarb16 <rgarb12@gmail.com>
2019-05-12 07:55:35 -0700

    Update config.py

--- iot_server_app/config.py
+++ iot_server_app/config.py
@@ -5,5 +5,6 @@ import urllib
 
 DATABASE = MongoClient()['sensordb'] # DB_NAME
 DEBUG = True
-uri = "mongodb://XXXX:"+urllib.quote("XXXX")+"@127.0.0.1/sensordb"
-client = MongoClient(uri)
+#uri = "mongodb://sensor_user1:"+urllib.quote("user@123")+"@127.0.0.1/sensordb"
+uri = "mongodb://sensor_user1:"+urllib.quote("user@123")+"@mongo1:27017,mongo2:27017/?replicaSet=myreplica01"
+client = MongoClient(['mongo1:27017', 'mongo2:27017', 'mongo3:27017'], replicaSet='myreplica01')
